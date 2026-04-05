1246dea90f37e33468bdebfcdf0d3866037b9ef0
tray <at-box@mail.ru>
2026-04-05 21:55:22 +0500

    category.py added

--- src/category.py
+++ src/category.py
@@ -1,3 +1,37 @@
 """module with Category class definition"""
 
+from src.product import Product
 
+class Category:
+    """
+    class to represent a category of products
+
+    Attributes:
+    categories_number: int
+        class attribute: number of categories
+    products_quantity: int
+        class attribute: total number of products in all categories
+
+    name: str
+        name of a category
+    description: str
+        description of a category
+    products: list[Product]
+        list of products associated with this category
+    """
+    categories_number: int = 0
+    products_quantity: int = 0
+
+
+    def __init__(self, name: str, description: str, products: list[Product]) -> None:
+        """
+        class constructor
+        :param name: str with name of a category
+        :param description: str with description of a category
+        :param products: list of Products class objects associated with this category
+        """
+        self.name = name
+        self.description = description
+        self.products = products
+        Category.categories_number += 1
+        Category.products_quantity += len(products)
