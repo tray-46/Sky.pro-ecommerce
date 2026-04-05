ffa17d3022d250902c5d91751f6639dadc3a75cb
tray <at-box@mail.ru>
2026-04-05 21:55:02 +0500

    product.py added

--- src/product.py
+++ src/product.py
@@ -1,3 +1,29 @@
 """module with Product class definition"""
 
+class Product:
+    """
+    class to represent a product
 
+    Attributes:
+    name: str
+        name of a product
+    description: str
+        description of a product
+    price: float
+        price of a product
+    quantity: int
+        quantity of a product
+    """
+
+    def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
+        """
+        Person class constructor
+        :param name: str with name of a product
+        :param description: str with description of a product
+        :param price: float with price of a product
+        :param quantity: int with quantity of a product
+        """
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
