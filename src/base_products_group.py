a8b270dd66acfa7a47cc492ea34b0d21dc96db11
tray <at-box@mail.ru>
2026-04-15 14:57:10 +0500

    base_products_group.py added

---
+++ src/base_products_group.py
@@ -0,0 +1,11 @@
+"""module with BaseProduct class definition"""
+
+from abc import ABC, abstractmethod
+
+class ProductsGroup(ABC):
+    """abstract class to represent a base products group"""
+
+    @property
+    @abstractmethod
+    def products(self) -> str:
+        """group products in string format"""
